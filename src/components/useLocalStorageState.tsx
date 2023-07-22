ed1eefe892aa2068ecc55f2b9faa4edfca34a92b
邓祥柏 <1760399176@qq.com>
2023-07-22 21:38:33 +0800

    chore: useless

--- src/components/useLocalStorageState.tsx
+++ src/components/useLocalStorageState.tsx
@@ -1,4 +1,4 @@
-import useLocalStorageState from "../hooks/state/useLocalStorageState"
+import useLocalStorageState from '../hooks/state/useLocalStorageState'
 
 export default function () {
   const [message, setMessage] = useLocalStorageState<string | undefined>(
@@ -20,6 +20,31 @@ export default function () {
       </button>
       <button type="button" onClick={() => setMessage(undefined)}>
         Clear
+      </button>
+
+      <WithComplex />
+    </>
+  )
+}
+
+const WithComplex = () => {
+  const defaultArray = ['a', 'e', 'i', 'o', 'u']
+  const [value, setValue] = useLocalStorageState('use-local-storage-state-demo2', {
+    defaultValue: defaultArray,
+  })
+
+  return (
+    <>
+      <p>{value?.join('-')}</p>
+      <button
+        type="button"
+        style={{ marginRight: '16px' }}
+        onClick={() => setValue([...value, Math.random().toString(36).slice(-1)])}
+      >
+        push random
+      </button>
+      <button type="button" onClick={() => setValue(defaultArray)}>
+        reset
       </button>
     </>
   )
