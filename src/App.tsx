657251cd027fc632845f86bda1cd786f3b2a53cc
邓祥柏 <1760399176@qq.com>
2023-07-22 12:56:49 +0800

    chore: Don't submit the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
+import { useState } from 'react'
 import './App.css'
 // import UseLatest from './components/useLatest'
-import UseUnmount from './components/useUnmount'
+// import UseUnmount from './components/useUnmount'
+// import UseDebounceFn from './components/useDebounceFn'
+// import UseLockFn from './components/useLockFn'
+// import UsePrevious from './components/usePrevious'
+// import UseToggle from './components/useToggle'
+// import UseUpdateEffect from './components/useUpdateEffect'
+// import UseAsyncEffect from './components/useAsyncEffect'
+import UseMemoizedFn from './components/useMemoizedFn'
+
 function App() {
+  const [state, setState] = useState(true)
   return (
     <>
-      <UseUnmount />
+      <button onClick={() => setState(!state)}> 切换状态（模拟组件卸载）</button>
+      {state && <UseMemoizedFn />}
     </>
   )
 }
