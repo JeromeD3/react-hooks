18234d7ad6b3f15321f0b4d1569614ff0965a061
邓祥柏 <1760399176@qq.com>
2023-08-02 20:51:18 +0800

    chore: ..

--- src/utils/domTarget.ts
+++ src/utils/domTarget.ts
@@ -1,9 +1,6 @@
 import type { MutableRefObject } from 'react'
 import { isBrowser } from '../hooks/useRequest/utils/isBrowser'
 
-
-
-
 export const isFunction = (value: unknown): value is (...args: any) => any => typeof value === 'function'
 
 type TargetValue<T> = T | undefined | null
@@ -13,7 +10,9 @@ type TargetType = HTMLElement | Element | Window | Document
 // 目标类型， 可以是一个element，也可以是一个函数，或者是一个ref
 export type BasicTarget<T extends TargetType = Element> = (() => TargetValue<T>) | TargetValue<T> | MutableRefObject<TargetValue<T>>
 
-// 返回目标元素
+/**
+ * 返回目标元素，如果是函数，就执行函数，如果是ref，就返回ref.current
+ *  */
 export function getTargetElement<T extends TargetType>(target: BasicTarget<T>, defaultElement?: T) {
   if (!isBrowser) {
     return undefined
